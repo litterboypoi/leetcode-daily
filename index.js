a1c5bdd20e8543dcdf9bfcdb7305ac6f1cd79547
Xiangbang Liang <yuudachidaisuki@outlook.com>
2019-12-11 10:04:53 +0800

    232. 用栈实现队列

--- index.js
+++ index.js
@@ -1192,3 +1192,66 @@ var findTargetSumWays = function(nums, S) {
     calc(0, pointer)
     return result
 };
+
+/**
+ * 232. 用栈实现队列
+ * https://leetcode-cn.com/problems/implement-queue-using-stacks/submissions/
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.stack = []
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack.push(x)
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if (this.empty()) {
+        return
+    }
+    const temp = []
+    let front
+    while (!this.empty()) {
+        temp.push(this.stack.pop())
+    }
+    front = temp.pop()
+    while(temp.length !== 0) {
+        this.push(temp.pop())
+    }
+    return front
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack[0]
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack.length === 0
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
