bfff0d32dcfc16b624c6bad98eabc392f1f6adc3
Liang XB <yuudachidaisuki@outlook.com>
2019-12-22 13:37:37 +0800

    234. 回文链表

--- week19.12.22-12.28.js
+++ week19.12.22-12.28.js
@@ -22,3 +22,44 @@ var reverseList = function(head) {
     }
     return prev
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * 234. 回文链表
+ * https://leetcode-cn.com/problems/palindrome-linked-list/submissions/
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let fast = head
+    let slow = head
+    // 通过快慢指针找到链表的中间节点
+    while (fast) {
+        slow = slow.next
+        fast = fast.next && fast.next.next
+    }
+    // 反转链表的后半部分
+    let prev = null
+    while (slow) {
+        const temp = slow.next
+        slow.next = prev
+        prev = slow
+        slow = temp
+    }
+
+    // check
+    while (head && prev) {
+        if (head.val !== prev.val) {
+            return false
+        }
+        head = head.next
+        prev = prev.next
+    }
+    return true
+};
