b208587b5523c41533f7bda2961a3c9378c30b22
Xiangbang Liang <yuudachidaisuki@outlook.com>
2019-12-29 16:01:16 +0800

    217. 存在重复元素

--- week19.1229-20.0104.js
+++ week19.1229-20.0104.js
@@ -82,3 +82,21 @@ MyHashMap.prototype.remove = function(key) {
 * var param_2 = obj.get(key)
 * obj.remove(key)
 */
+
+/**
+ * 217. 存在重复元素
+ * https://leetcode-cn.com/problems/contains-duplicate/
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  const mySet = new Set()
+  for (let i = 0; i < nums.length; i++) {
+      if (mySet.has(nums[i])) {
+          return true
+      } else {
+          mySet.add(nums[i])
+      }
+  }
+  return false
+};
