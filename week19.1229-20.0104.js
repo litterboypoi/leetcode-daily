282ba25afc9b7dde51d2bdd328b2bc0af04cb7d6
Xiangbang Liang <yuudachidaisuki@outlook.com>
2019-12-30 18:23:52 +0800

    387. 字符串中的第一个唯一字符

--- week19.1229-20.0104.js
+++ week19.1229-20.0104.js
@@ -193,3 +193,22 @@ var isIsomorphic = function(s, t) {
   }
   return true
 };
+
+/**
+ * 387. 字符串中的第一个唯一字符
+ * https://leetcode-cn.com/problems/first-unique-character-in-a-string/
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  const m = new Map()
+  for (let i = 0; i < s.length; i++) {
+      m.set(s.charAt(i), m.get(s.charAt(i)) ? s.charAt(i) + 1 : 1)
+  }
+  for (let i = 0; i < s.length; i++) {
+      if (m.get(s.charAt(i)) === 1) {
+          return i
+      }
+  }
+  return -1
+};
