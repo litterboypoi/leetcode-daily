50132a5de6f46ca686bd739a006ec1657c0a6ccd
Xiangbang Liang <yuudachidaisuki@outlook.com>
2019-12-30 18:00:41 +0800

    205. 同构字符串

--- week19.1229-20.0104.js
+++ week19.1229-20.0104.js
@@ -168,3 +168,28 @@ var isHappy = function(n) {
       mySet.add(n)
   }
 };
+
+/**
+ * 205. 同构字符串
+ * https://leetcode-cn.com/problems/isomorphic-strings/
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+  if (s.length !== t.length) {
+      return false
+  }
+  const map1 = new Map()
+  const map2 = new Map()
+  for (let i = 0; i < s.length; i++) {
+      map1.set(s[i], t[i])
+      map2.set(t[i], s[i])
+  }
+  for (let i = 0; i < s.length; i++) {
+      if (map1.get(s[i]) !== t[i] || map2.get(t[i]) !== s[i]) {
+          return false
+      }
+  }
+  return true
+};
