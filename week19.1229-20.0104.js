080958a36a822545371cf311b400105cb076934d
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-02 16:39:34 +0800

    3. 无重复字符的最长子串

--- week19.1229-20.0104.js
+++ week19.1229-20.0104.js
@@ -365,3 +365,26 @@ var numJewelsInStones = function(J, S) {
   }
   return result
 };
+
+/**
+ * 3. 无重复字符的最长子串
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  const map = new Map()
+  let start  = 0
+  let max = 0
+  for (let i = 0; i < s.length; i++) {
+      const char = s.charAt(i)
+      if (map.has(char)) {
+          start = Math.max(map.get(char) + 1, start)
+      }
+      max = Math.max(max, i - start + 1)
+      map.set(char, i)
+  }
+  return max
+};
+
+lengthOfLongestSubstring('tmmzuxt')
