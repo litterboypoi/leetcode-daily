4308568e8574c1046897285056dba174be0749b9
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-06 15:56:58 +0800

    374. 猜数字大小

--- 2020/week01.js
+++ 2020/week01.js
@@ -26,4 +26,26 @@ var mySqrt = function(x) {
   }
 };
 
-mySqrt(8)
+// 374. 猜数字大小
+// https://leetcode-cn.com/problems/guess-number-higher-or-lower/
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+    //   public class Solution extends GuessGame {
+    //     public int guessNumber(int n) {
+    //         int left = 1, right = n;
+    //         while (left <= right) {
+    //             int mid = left + (right - left) / 2;
+    //             int r = guess(mid);
+    //             if (r == 0) {
+    //                 return mid;
+    //             } else if (r == 1) {
+    //                 left = mid + 1;
+    //             } else {
+    //                 right = mid - 1;
+    //             }
+    //         }
+    //         return 0;
+    //     }
+    // }
