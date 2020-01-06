928b75d2b78e7d8b40f9258a433c9a48faa6e401
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-06 16:57:29 +0800

    162. 寻找峰值

--- 2020/week01.java
+++ 2020/week01.java
@@ -17,3 +17,20 @@
             return r;
         }
     }
+
+    // https://leetcode-cn.com/problems/find-peak-element/
+    // 162. 寻找峰值
+    class Solution {
+      public int findPeakElement(int[] nums) {
+          int l = 0, r = nums.length - 1;
+          while (l < r) {
+              int mid = l + (r - l) / 2;
+              if (nums[mid] > nums[mid + 1]) {
+                  r = mid;
+              } else {
+                  l = mid + 1;
+              }
+          }
+          return l;
+      }
+  }
