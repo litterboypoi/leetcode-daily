f3839b1f2c986a5cae5217e014b76fa2c510c556
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-06 15:58:15 +0800

    33. 搜索旋转排序数组

--- 2020/week01.js
+++ 2020/week01.js
@@ -49,3 +49,31 @@ var mySqrt = function(x) {
     //         return 0;
     //     }
     // }
+
+// 33. 搜索旋转排序数组
+// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int l = 0, r = nums.length - 1;
+//         while(l <= r) {
+//             int mid = (l + r) / 2;
+//             if (nums[mid] == target) {
+//                 return mid;
+//             }
+//             if (nums[l] > nums[mid]) {
+//                 if (nums[mid] < target && nums[r] >= target) {
+//                     l = mid + 1;
+//                 } else {
+//                     r = mid - 1;
+//                 }
+//             } else {
+//                 if (nums[l] <= target && nums[mid] > target) {
+//                     r = mid - 1;
+//                 } else {
+//                     l = mid + 1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// }
