cc5c74260e93fff265c50e1f8f14eb3530b2aa6c
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-06 18:17:35 +0800

    153. 寻找旋转排序数组中的最小值

--- 2020/week01.java
+++ 2020/week01.java
@@ -34,3 +34,35 @@
           return l;
       }
   }
+
+  // 153. 寻找旋转排序数组中的最小值
+  // https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+  class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            // 错误原因：当左右两边都是有序的时候，最小值应该在左边，然而现在的代码会优先把l推向右边，而不是把r推向左边
+            if (nums[l] < nums[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+}
+class Solution {
+  public int findMin(int[] nums) {
+      int l = 0, r = nums.length - 1;
+      while (l < r) {
+          int mid = l + (r - l) / 2;
+          if (nums[mid] < nums[r]) {
+              r = mid;
+          } else {
+              l = mid + 1;
+          }
+      }
+      return nums[l];
+  }
+}
