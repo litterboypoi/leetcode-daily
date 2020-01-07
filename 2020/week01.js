bea0b07ed581f2cc0f64e46f64025de2b9d7a33a
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-07 15:52:19 +0800

    6. Z 字形变换

--- 2020/week01.js
+++ 2020/week01.js
@@ -77,3 +77,28 @@ var mySqrt = function(x) {
 //         return -1;
 //     }
 // }
+
+/**
+ * 6. Z 字形变换
+ * https://leetcode-cn.com/problems/zigzag-conversion/
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  if (numRows === 1) {
+      return s
+  }
+  let strList = Array(Math.min(s.length, numRows)).fill('')
+  let curRow = 0
+  let isGoingDown = false
+  let c = 2 * (numRows - 1)
+  for (let i = 0; i < s.length; i++) {
+      strList[curRow] += s.charAt(i)
+      if (i % c === 0 || i % c === numRows - 1 ) {
+          isGoingDown = !isGoingDown
+      }
+      curRow = isGoingDown ? curRow + 1 : curRow - 1
+  }
+  return strList.join('')
+};
