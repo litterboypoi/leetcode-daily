d1b6104d1d06a72fee3b3bdfdb466f54aa71458e
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-09 18:13:39 +0800

    88. 合并两个有序数组

--- 2020/week01.js
+++ 2020/week01.js
@@ -229,3 +229,36 @@ var maxArea = function(height) {
   }
   return max
 };
+
+/**
+ * 88. 合并两个有序数组
+ * https://leetcode-cn.com/problems/merge-sorted-array/
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  let i = m - 1
+  let j = n - 1
+  let p = m+n-1
+  while (i >= 0 || j >= 0) {
+      if (i < 0) {
+          nums1[p] = nums2[j]
+          j--
+      } else if (j < 0) {
+          nums1[p] = nums1[i]
+          i--
+      } else {
+          if (nums1[i] > nums2[j]) {
+              nums1[p] = nums1[i]
+              i--
+          } else {
+              nums1[p] = nums2[j]
+              j--
+          }
+      }
+      p--
+  }
+};
