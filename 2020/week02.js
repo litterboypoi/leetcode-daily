ea0d8d93013e0c8863036429a2855bc865c00e35
Xiangbang Liang <yuudachidaisuki@outlook.com>
2020-01-18 10:21:02 +0800

    13. 罗马数字转整数

--- 2020/week02.js
+++ 2020/week02.js
@@ -394,3 +394,29 @@ var isPowerOfThree = function(n) {
   }
   return false
 };
+
+/**
+ * 13. 罗马数字转整数
+ * https://leetcode-cn.com/problems/roman-to-integer/
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  let map = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  }
+  let num = 0
+  for (let i = 0; i < s.length; i++) {
+      num += map[s.charAt(i)]
+      if (i > 0 && map[s.charAt(i)] > map[s.charAt(i - 1)]) {
+          num -= 2 * map[s.charAt(i - 1)]
+      }
+  }
+  return num
+};
