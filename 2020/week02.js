9f6ae950824b869c53434808da95c988b9c62538
Liang XB <yuudachidaisuki@outlook.com>
2020-01-23 22:18:18 +0800

    17. 电话号码的字母组合

--- 2020/week02.js
+++ 2020/week02.js
@@ -611,3 +611,37 @@ var kthSmallest = function(root, k) {
     transver(root)
     return res
 };
+
+/**
+ * 17. 电话号码的字母组合
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (!digits) {
+        return []
+    }
+    let map = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    }
+    let result = []
+    function digitToLetter(prefix, digitIdx) {
+        if (digitIdx === digits.length) {
+            result.push(prefix)
+            return
+        }
+        for (let letter of map[digits[digitIdx]]) {
+            digitToLetter(prefix + letter, digitIdx + 1)
+        }
+    }
+    digitToLetter('', 0)
+    return result
+};
